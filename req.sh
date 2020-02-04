5b59347ace20ef1f0d60383831e8c035884a8a72
Bhaskar <sharmabhaskar13@gmail.com>
2020-02-04 20:06:38 +0530

    fix script

---
+++ req.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone https://github.com/bhaskar20/wiki_corpus_doc2vec_exp.git
+cd wiki_corpus_doc2vec_exp
+git config --global user.name "bhaskar20"
+git config --global user.email "sharmabhaskar13@gmail.com"
+git pull origin master
+mkdir modelsData
+mv ../gensim/modelsData .
